9a6d46bdf5170dc08531d3fc64af1a7d52e81c1f
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-10-12 21:22:04 +0300

    change redirect route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -42,8 +42,8 @@ export const App = () => {
             path="/:verificationToken"
             element={
               <RestrictedRoute
-                redirectTo="/contacts"
-                component={<ContactsPage />}
+                redirectTo="/login"
+                component={<LoginPage />}
               />
             }
           />
